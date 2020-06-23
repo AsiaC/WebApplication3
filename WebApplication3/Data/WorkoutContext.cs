d7379c181e247367b9e2a4399335f038414aab05
AsiaC <czaplicka.asia@gmail.com>
2020-06-23 14:38:15 +0200

    Add project files.

---
+++ WebApplication3/Data/WorkoutContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+    public class WorkoutContext : DbContext
+    {
+        public WorkoutContext (DbContextOptions<WorkoutContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication3.Models.Workout> Workout { get; set; }
+    }
